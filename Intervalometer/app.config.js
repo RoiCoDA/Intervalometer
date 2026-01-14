89179393ac22dc1e94b9a20e5a4fbc443db0e499
RoiCoDA <roishm83@gmail.com>
2026-01-14 11:36:10 +0200

    Build fix attempt v3

---
+++ Intervalometer/app.config.js
@@ -0,0 +1,63 @@
+const { withAndroidManifest } = require('@expo/config-plugins');
+
+// The Script: Forces Android to accept the conflict
+const withAndroidToolsPatch = (config) => {
+  return withAndroidManifest(config, async (config) => {
+    const manifest = config.modResults.manifest;
+    const mainApplication = manifest.application[0];
+
+    manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
+
+    mainApplication.$['tools:replace'] = 'android:appComponentFactory';
+
+    return config;
+  });
+};
+
+module.exports = {
+  expo: {
+    name: "Intervalometer",
+    slug: "Intervalometer",
+    version: "1.0.0",
+    orientation: "portrait",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true, 
+    ios: {
+      supportsTablet: true,
+      infoPlist: {
+        NSMicrophoneUsageDescription: "Allow the app to listen for the 'Go' command to advance steps.",
+        NSSpeechRecognitionUsageDescription: "Allow the app to recognize your voice for the 'Go' command."
+      }
+    },
+    android: {
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      permissions: [
+        "android.permission.RECORD_AUDIO",
+        "android.permission.INTERNET"
+      ],
+      package: "com.awesomeprojectboris.Intervalometer"
+    },
+    web: {
+      output: "static"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-build-properties",
+        {
+          android: {
+            useAndroidX: true,
+            enableJetifier: true 
+          }
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
+    }
+  }
+};
+
+module.exports.expo = withAndroidToolsPatch(module.exports.expo);
